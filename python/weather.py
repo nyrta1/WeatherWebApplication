f1a66eff60569e5a20b8d177ef8bd067fe86c637
nyrta1 <nurtai_2005@mail.ru>
2023-12-30 20:08:39 +0600

    feat: added video animation from youtube. References in the folder video as named references.txt

--- python/weather.py
+++ python/weather.py
@@ -3,4 +3,5 @@ import python_weather
 async def getCurrentWeather(city):
     async with python_weather.Client(unit=python_weather.METRIC) as client:
         weather = await client.get(city)
+
         return weather
--- react-vite/src/components/weather_card.tsx
+++ react-vite/src/components/weather_card.tsx
@@ -56,58 +56,97 @@ export default function WeatherCard() {
     return (
         <>
             {/*REFERENCE SITE: https://bbbootstrap.com/snippets/complete-weather-report-search-bar-32715352*/}
-            <div className="container-fluid px-1 px-sm-2 py-5 mx-auto">
-                <div className="row d-flex justify-content-center">
-                    <div className="row card0">
-                        <div className={`card1 col-lg-8 col-md-7 ${getLinearGradientColor(weatherInfo?.kind)} `}>
-                            <small>WeatherApp</small>
-                            <div className="text-center">
-                                <img className="image mt-5" src="https://i.imgur.com/M8VyA2h.png" />
-                            </div>
-                            <div className="row px-3 mt-3 mb-3">
-                                <h1 className="large-font mr-3">{weatherInfo?.temperature}°C</h1>
-                                <div className="d-flex flex-column mr-3">
-                                    <h2 className="mt-3 mb-0">{weatherInfo?.city}, {weatherInfo?.country}</h2>
-                                    <small>{weatherInfo?.date}</small>
+            <div className="video-container">
+                <video autoPlay muted loop>
+                    <source src="./src/assets/videos/video.mp4" type="video/mp4" />
+                </video>
+                <div className="caption">
+                    <div className="container-fluid px-1 px-sm-2 py-2 mx-auto vh-100">
+                        <div className="row d-flex justify-content-center">
+                            <div className="row card0">
+                                <div className={`card1 col-lg-8 col-md-7 ${getLinearGradientColor(weatherInfo?.kind)} `}>
+                                    <small>WeatherApp</small>
+                                    <div className="text-center">
+                                        <img className="image mt-5" src="https://i.imgur.com/M8VyA2h.png" />
+                                    </div>
+                                    <div className="row px-3 mt-3 mb-3">
+                                        <h1 className="large-font mr-3">{weatherInfo?.temperature}°C</h1>
+                                        <div className="d-flex flex-column mr-3">
+                                            <h2 className="mt-3 mb-0">{weatherInfo?.city}, {weatherInfo?.country}</h2>
+                                            <small>{weatherInfo?.date}</small>
+                                        </div>
+                                        <div className="d-flex flex-column text-center mt-5">
+                                            <WeatherIcon kind={weatherInfo?.kind}></WeatherIcon>
+                                            <small>{weatherInfo?.kind}</small>
+                                        </div>
+                                    </div>
                                 </div>
-                                <div className="d-flex flex-column text-center">
-                                    <WeatherIcon kind={weatherInfo?.kind}></WeatherIcon>
-                                    <small>{weatherInfo?.kind}</small>
+                                <div className="card2 col-lg-4 col-md-5">
+                                    <div className="row px-3">
+                                        <input type="text" name="location" placeholder="Another location" className="col-9 mb-4" value={cityName || ''} onChange={getCityNameFromInput} />
+                                        <div className="col-2 bi bi-search mb-4 mr-0 text-center" onClick={fetchData}></div>
+                                    </div>
+                                    <div className="mr-5">
+                                        <p>Weather Details</p>
+                                        <div className="row px-3">
+                                            <p className="light-text">Feels like</p>
+                                            <p className="ml-auto">{weatherInfo?.feels_like}°C</p>
+                                        </div>
+                                        <div className="row px-3">
+                                            <p className="light-text">Humidity</p>
+                                            <p className="ml-auto">{weatherInfo?.humidity}%</p>
+                                        </div>
+                                        <div className="row px-3">
+                                            <p className="light-text">Wind</p>
+                                            <p className="ml-auto">{(Number((weatherInfo?.wind_speed! / 3.6).toFixed(1)))} m/s</p>
+                                        </div>
+                                        <div className="row px-3">
+                                            <p className="light-text">Wind Direction</p>
+                                            <p className="ml-auto">{weatherInfo?.wind_direction}</p>
+                                        </div>
+                                        <div className="row px-3">
+                                            <p className="light-text">Pressure</p>
+                                            <p className="ml-auto">{weatherInfo?.pressure} atm</p>
+                                        </div>
+                                        <div className="row px-3">
+                                            <p className="light-text">Rain</p>
+                                            <p className="ml-auto">{weatherInfo?.precipitation}mm</p>
+                                        </div>
+                                        <div className="line mt-3"></div>
+                                    </div>
+                                    <div className="d-flex justify-content-around align-items-center pt-3">
+                                        <div className="flex-column">
+                                            <i className="fas fa-minus"></i>
+                                        </div>
+                                        <div className="flex-column">
+                                            <p className="small mb-1">Sun</p>
+                                            <p className="small mb-0"><strong>0°C</strong></p>
+                                        </div>
+                                        <div className="flex-column">
+                                            <p className="small mb-1">Mon</p>
+                                            <p className="small mb-0"><strong>0°C</strong></p>
+                                        </div>
+                                        <div className="flex-column">
+                                            <p className="small mb-1">Tue</p>
+                                            <p className="small mb-0"><strong>0°C</strong></p>
+                                        </div>
+                                        <div className="flex-column">
+                                            <p className="small mb-1">Wed</p>
+                                            <p className="small mb-0"><strong>0°C</strong></p>
+                                        </div>
+                                        <div className="flex-column">
+                                            <p className="small mb-1">Thu</p>
+                                            <p className="small mb-0"><strong>0°C</strong></p>
+                                        </div>
+                                        <div className="flex-column">
+                                            <p className="small mb-1">Thu</p>
+                                            <p className="small mb-0"><strong>0°C</strong></p>
+                                        </div>
+                                        <div className="flex-column">
+                                            <i className="fas fa-minus"></i>
+                                        </div>
+                                    </div>
                                 </div>
-                            </div>
-                        </div>
-                        <div className="card2 col-lg-4 col-md-5">
-                            <div className="row px-3">
-                                <input type="text" name="location" placeholder="Another location" className="col-9 mb-5" value={cityName || ''} onChange={getCityNameFromInput} />
-                                <div className="col-2 bi bi-search mb-5 mr-0 text-center" onClick={fetchData}></div>
-                            </div>
-                            <div className="mr-5">
-                                <p>Weather Details</p>
-                                <div className="row px-3">
-                                    <p className="light-text">Feels like</p>
-                                    <p className="ml-auto">{weatherInfo?.feels_like}°C</p>
-                                </div>
-                                <div className="row px-3">
-                                    <p className="light-text">Humidity</p>
-                                    <p className="ml-auto">{weatherInfo?.humidity}%</p>
-                                </div>
-                                <div className="row px-3">
-                                    <p className="light-text">Wind</p>
-                                    <p className="ml-auto">{(Number((weatherInfo?.wind_speed! / 3.6).toFixed(1)))} m/s</p>
-                                </div>
-                                <div className="row px-3">
-                                    <p className="light-text">Wind Direction</p>
-                                    <p className="ml-auto">{weatherInfo?.wind_direction}</p>
-                                </div>
-                                <div className="row px-3">
-                                    <p className="light-text">Pressure</p>
-                                    <p className="ml-auto">{weatherInfo?.pressure} atm</p>
-                                </div>
-                                <div className="row px-3">
-                                    <p className="light-text">Rain</p>
-                                    <p className="ml-auto">{weatherInfo?.precipitation}mm</p>
-                                </div>
-                                <div className="line mt-3"></div>
                             </div>
                         </div>
                     </div>
